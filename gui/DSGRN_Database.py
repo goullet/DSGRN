6f1ccfb47121c9942b1904914bfcccd1c45fd96a
Arnaud Goullet <arnaud.goullet@gmail.com>
2016-02-24 22:34:28 -0500

    Started to work on the DSGRN GUI

--- gui/DSGRN_Database.py
+++ gui/DSGRN_Database.py
@@ -4,9 +4,13 @@ import sqlite3
 def connect ( name ):
     return sqlite3.connect( name )
 
+def query ( connector, SQLcommand ):
+    return connector.execute(SQLcommand).fetchall()
+
 def network ( connector ):
-    name = connector.execute("SELECT Name FROM Network;").fetchone()                   # Name of the network
+    # name = connector.execute("SELECT Name FROM Network;").fetchone()                   # Name of the network
+    name = query( connector, "SELECT Name FROM Network;" )
     dimension = connector.execute("SELECT Dimension FROM Network;").fetchone()         # Number of nodes in the network
     specification = connector.execute("SELECT Specification FROM Network;").fetchall() # Equations solved
     graphviz_text=connector.execute("SELECT Graphviz FROM Network;").fetchall()        # Graphviz network code
-    return [name[0],dimension[0],specification[0][0],graphviz_text[0][0]]
+    return [name[0][0],dimension[0],specification[0][0],graphviz_text[0][0]]
