78667b936e4ce35c3cceee8ee45b0a2a5402f2d6
Arnaud Goullet <arnaud.goullet@gmail.com>
2016-05-30 17:04:48 -0400

    started to add docstring to the python classes/methods.

--- software/Python/source/pyDSGRN.cpp
+++ software/Python/source/pyDSGRN.cpp
@@ -8,7 +8,7 @@
 
 // Network wrapping incidentals
 
-// IntList 
+// IntList
 typedef std::vector<uint64_t> IntList;
 std::string
 printIntList ( IntList const* self ) {
@@ -96,6 +96,10 @@ BOOST_PYTHON_MODULE(libpyDSGRN)
 {
     using namespace boost::python;
 
+    // This will enable user-defined docstrings and python signatures,
+      // while disabling the C++ signatures
+      docstring_options local_docstring_options(true, true, false);
+
     // Network
 
     class_<IntList>("IntList")
@@ -110,23 +114,79 @@ BOOST_PYTHON_MODULE(libpyDSGRN)
       .def("__repr__", &printIntListList)
     ;
 
-    class_<Network>("Network", init<std::string>())
+    class_<Network>("Network", "Network Class", init<std::string>())
       .def(init<>())
-      .def("load", &Network::load)  
-      .def("assign", &Network::assign)  
-      .def("size", &Network::size)  
-      .def("index", &Network::index)  
-      .def("name", &Network::name, return_value_policy<copy_const_reference>()) 
-      .def("inputs", &Network::inputs, return_value_policy<copy_const_reference>()) 
-      .def("outputs", &Network::inputs, return_value_policy<copy_const_reference>()) 
-      .def("logic", &Network::logic, return_value_policy<copy_const_reference>())
-      .def("essential", &Network::essential) 
-      .def("interaction", &Network::interaction) 
-      .def("order", &Network::order) 
-      .def("domains", &Network::domains) 
-      .def("specification", &Network::specification)
-      .def("graphviz", &Network::graphviz, network_overloads()) 
-      .def("__str__", &Network::specification)
+      .def( "load",
+            &Network::load,
+            "Load a network from a file given by filename."
+            , args("self","filename")
+          )
+      .def( "assign",
+            &Network::assign
+          )
+      .def( "size",
+            &Network::size,
+            "Return the number of nodes in the network",
+            args("self")
+          )
+      .def( "index",
+            &Network::index,
+            "Return the node index of a given node variable name",
+            args("self","node name")
+          )
+      .def( "name",
+            &Network::name,
+            "Return the name of the node of a given node index",
+            return_value_policy<copy_const_reference>(),
+            args("self", "node index")
+          )
+      .def( "inputs",
+            &Network::inputs,
+            "Return the list of inputs of a given node",
+            return_value_policy<copy_const_reference>(),
+            args("self", "node index")
+          )
+      .def( "outputs",
+            &Network::inputs,
+            "Return the list of outputs of a given node",
+            return_value_policy<copy_const_reference>(),
+            args("self", "node index")
+          )
+      .def( "logic",
+            &Network::logic,
+            "Return the logic of a given node",
+            return_value_policy<copy_const_reference>(),
+            args("self", "node index")
+          )
+      .def( "essential",
+            &Network::essential,
+            "Return whether or not to use only essential parameters for a given node",
+            args("self", "node index")
+          )
+      .def( "interaction",
+            &Network::interaction,
+            "Return the interaction type of an edge (False for depression, True for activation)",
+            args("self", "source index", "target index")
+          )
+      .def( "order",
+            &Network::order,
+            "Return the out-edge order number of an edge, \ni.e. so outputs(source)[order(source,target)] == target",
+            args("self", "source index", "target index")
+          )
+      .def( "domains",
+            &Network::domains,
+            "Return a list consisting of the number of domains across (i.e. number of out-edges + 1) \nfor each dimension (i.e. network node)",
+            args("self")
+          )
+      .def( "specification",
+            &Network::specification,
+            "Return the specification string of the network",
+            args("self")
+          )
+      .def( "graphviz",
+            &Network::graphviz,
+            network_overloads( "Return a graphviz string (dot language) of the network", args("self") ) )
+      .def( "__str__", &Network::specification)
     ;
 
     // LogicParameter
@@ -139,7 +199,7 @@ BOOST_PYTHON_MODULE(libpyDSGRN)
 
     class_<LogicParameter>("LogicParameter", init<uint64_t, uint64_t, std::string>())
       .def(init<>())
-      .def("assign", &LogicParameter::assign)  
+      .def("assign", &LogicParameter::assign)
       .def("__call__",static_cast<bool (LogicParameter::*)( std::vector<bool> const&, uint64_t ) const>
          (&LogicParameter::operator()) ) // How is python user going to get hands on vector<bool> ? TODO
       .def("__call__",static_cast<bool (LogicParameter::*)( uint64_t ) const>
@@ -214,20 +274,74 @@ BOOST_PYTHON_MODULE(libpyDSGRN)
 
     class_<Digraph>("Digraph", init<>())
       .def(init<std::vector<std::vector<uint64_t>>&>())
-      .def("assign", &Digraph::assign)
-      .def("adjacencies", static_cast<std::vector<uint64_t> const& (Digraph::*)(uint64_t)const>(&Digraph::adjacencies), return_value_policy<copy_const_reference>())
-      .def("size", &Digraph::size)
-      .def("resize", &Digraph::resize)
-      .def("add_vertex", &Digraph::add_vertex)
-      .def("add_edge", &Digraph::add_edge)
-      .def("finalize", &Digraph::finalize)
-      .def("transpose", &Digraph::transpose)
-      .def("transitive_reduction", &Digraph::transitive_reduction)
-      .def("transitive_closure", &Digraph::transitive_closure)
-      .def("permute", &Digraph::permute)
-      .def("stringify", &Digraph::stringify)
-      .def("parse", &Digraph::parse)
-      .def("graphviz", &Digraph::graphviz)
+      .def( "assign",
+            &Digraph::assign,
+            "Construct a digraph from an adjacency list",
+            args("self", "adjancency list")
+          )
+      .def( "adjacencies",
+            static_cast<std::vector<uint64_t> const& (Digraph::*)(uint64_t)const>(&Digraph::adjacencies), return_value_policy<copy_const_reference>()
+          )
+      .def( "size",
+            &Digraph::size,
+            "Return the number of vertives",
+            args("self")
+          )
+      .def( "resize",
+            &Digraph::resize,
+            "Resize the digraph to have n vertices.\nCreate empty adjacency list if needed.\nn should be greater the current size",
+            args("self", "n: new size")
+          )
+      .def( "add_vertex",
+            &Digraph::add_vertex,
+            "Add a vertex and return the index of the newly added vertex",
+            args("self")
+          )
+      .def( "add_edge",
+            &Digraph::add_edge,
+            "Add an edge between source and target.\nWarning: Does not check if the edge already exists",
+            args("self", "source index", "target index")
+          )
+      .def( "finalize",
+            &Digraph::finalize,
+            "Sort the adjacency list in ascending order",
+            args("self")
+          )
+      .def( "transpose",
+            &Digraph::transpose,
+            "Return the transpose graph.",
+            args("self")
+          )
+      .def( "transitive_reduction",
+            &Digraph::transitive_reduction,
+            "Return the transitive reduction\nNote: requires the graph be a transitively closed DAG \nor else a transitively closed DAG with extra self-edges",
+            args("self")
+          )
+      .def( "transitive_closure",
+            &Digraph::transitive_closure,
+            "Compute the transitive closure.\nNote: self-edges are not in the result even if they are in the input",
+            args("self")
+          )
+      .def( "permute",
+            &Digraph::permute,
+            "Reorder the digraph according to the provided permutation.\nThe convention on the permutation is that vertex v in the input \ncorresponds to vertex permutation[v] in the output",
+            args("self","permutation vector")
+          )
+      .def( "stringify",
+            &Digraph::stringify,
+            "Return a JSON description of the digraph.",
+            args("self")
+          )
+      .def( "parse",
+            &Digraph::parse,
+            "Initialize a digraph from a JSON description.\nFormat: an array of arrays of integers.\nThe inner arrays are adjacency lists.\nVertices are numbered 0...N-1\nThe outer array has length N",
+            args("self", "JSON string" )
+          )
+      .def( "graphviz",
+            &Digraph::graphviz,
+            "Return a Graphviz representation (dot language) of the digraph",
+            args("self")
+          )
       .def("__str__", &Digraph::graphviz)
     ;
 
